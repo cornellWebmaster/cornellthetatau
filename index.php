c726085d3a50b8e2ccde7e0c47c6ed0c76fa3480
cleung11 <cpl28@cornell.edu>
2017-01-24 21:27:48 -0500

    commented form for testing

--- index.php
+++ index.php
@@ -158,7 +158,13 @@
 	      <p class="h6 regular grey caption mb0 ">4. Information Session attendance is mandatory.</p>
 	      <br>
 	      <p class="h6 regular grey caption mb0 "> If you have a conflict or any questions/comments contact recruitment chair Lindsey Frank at Lrf52@cornell.edu</p>
+          <br>
+          <h6 class="mb0">Please check back soon for the application. In the meantime, we invite you to attend one of our information sessions on Monday, January 30 at 7pm in Olin 155 or Tuesday, January 31 at 7pm in Olin 155.
+          </h6>
 	</div>
+
+	<?php
+	/*
 	<div class="p4">
 		<a name="form200217362" id="formAnchor200217362"></a>
 		<script type="text/javascript" src="https://fs12.formsite.com/include/form/embedManager.js?200217362"></script>
@@ -170,6 +176,9 @@
 		});
 		</script>
 	</div>
+	*/
+	?>
+
 </div>
 	
 	<!-- dont use the form below me use the one above me :) -->
